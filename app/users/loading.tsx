e8b27c48fb596fe1ec89e9b0ad923cfa1a58d7c8
abhijivani3001 <abhijivani3001@gmail.com>
2024-05-07 10:44:20 +0530

    loading modal added

---
+++ app/users/loading.tsx
@@ -0,0 +1,8 @@
+import LoadingModal from '../components/LoadingModal';
+
+const Loading = () => {
+  // Next js has a built-in loading component that can be used to show a loading spinner while the page is loading.
+  return <LoadingModal />;
+};
+
+export default Loading;
